f9f31aea89617b55298a2c37273fbccb9e6da4c8
杨传慧Vivian <48340401+alitaurm99@users.noreply.github.com>
2019-10-28 09:40:45 +0800

    Create GetArea.java

---
+++ GetArea.java
@@ -0,0 +1,24 @@
+public class GetArea{
+    public static double getAllCircleArea(Circle[] clist){
+        double sum = 0;
+        for(int i = 0; i < clist.length; i++){
+            sum += clist[i].getArea();
+        }
+        return sum;
+    }
+    public static double getAllRectangleArea(ArrayList<Rectangle> clist){
+        double sum = 0;
+        for(Rctangle r : clist){
+            sum += r.getArea();
+        }
+        return sum;
+    }
+    public static double getAllTriangleArea(Triangle[][] clist){
+        double sum = 0;
+        for(Triangle[] r : clist){
+            for(Triangle t : r){
+                sum += t.getArea();
+            }
+        }
+        return sum;
+    }
